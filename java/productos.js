20922f6ff0879e910c3453a4016bbfad48c28fbe
benja fontenla <bfontenla5@gmail.com>
2022-10-18 23:43:41 -0300

    segundaentrega

---
+++ java/productos.js
@@ -0,0 +1,31 @@
+const productos = [{
+        id: 1,
+        imagen: './fotos/urban.jpg',
+        nombre: 'Remera urban',
+        precio: 2000
+    },
+    {
+        id: 2,
+        imagen: './fotos/nfl2.jpg',
+        nombre: 'Camiseta nfl',
+        precio: 6000
+    },
+    {
+        id: 3,
+        imagen: './fotos/sale1.JPG',
+        nombre: 'Remera nike',
+        precio: 3500
+    },
+    {
+        id: 4,
+        imagen: './fotos/nfl.jpg',
+        nombre: 'Promo 2 Camiseta nfl',
+        precio: 10000
+    },
+    {
+        id: 5,
+        imagen: './fotos/conjunto.jpg',
+        nombre: 'Conjunto adidas',
+        precio: 9500
+    },
+]
--- java/script.js
+++ java/script.js
@@ -1,91 +1,49 @@
-//Pre entrega
-/*
-for (let i = 1; i <= 3; i++) {
-    let usuario = prompt('Ingrese nombre de Usuario');
-    let numero = parseInt(prompt('Ingrese contraseña'));
-    if ((usuario == 'benja') && (numero == '123456')) {
-        alert('Usuario y contraseña correcta');
-        break;
-    } else {
-        alert('Usuario y/o contraseña incorrecta restan ' + (3 - i) + ' intentos');
+const carrito = [];
+let totalCarrito;
+let contenedor = document.getElementById("Mis productos");
+
+
+function catalogo() {
+    for (const producto of productos) {
+        //Uso una plantilla literal
+        contenedor.innerHTML += `
+            <div class="card col-sm-2">
+                <img src=${producto.imagen} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.id}</h5>
+                    <p class="card-text">${producto.nombre}</p>
+                    <p class="card-text">$ ${producto.precio}</p>
+                    <button id="btn${producto.id}" class="btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        `;
     }
+
+
+    productos.forEach(producto => {
+        //evento para cada boton
+        document.getElementById(`btn${producto.id}`).addEventListener("click", function () {
+            agregarAlCarrito(producto);
+        });
+    })
 }
 
+catalogo();
 
 
 
-console.log('Estadisiticas de jugadores del futbol argentino')
-let estadisticas = prompt('Ingrese el apellido del futbolista que desea buscar o la letra z para salir');
-
-while (estadisticas != 'z') {
-    switch (estadisticas) {
-        case 'retegui':
-            console.log('Mateo Retegui es el maximo anotador del futbol argentino con 15 goles en 22 partidos');
-            break;
-
-        case 'cristaldo':
-            console.log('Cristaldo convirtio 11 goles en 21 aprtidos con un premedio de 0.52');
-            break;
-
-        case 'copetti':
-            console.log('Copetti convirtio 8 tantos en 20 partidos');
-            break;
-
-        case 'benedetto':
-            console.log('Benedetto marco 4 tantos en 12 partidos');
-            break;
-
-        case 'boselli':
-            console.log('Mauro Boselli convirtio 6 tantos en 17 partidos');
-            break;
-
-        default:
-            console.log('El futbolista que esta buscando no se encuentra en nuestra lista de datos');
-            break;
-    }
-    estadisticas = prompt('Ingrese el apellido del futbolista que desea buscar o la letra z para salir');
+function agregarAlCarrito(productoComprado) {
+    carrito.push(productoComprado);
+    console.table(carrito);
+    alert("Producto: " + productoComprado.nombre + " agregado al carrito!");
+    document.getElementById("tablabody").innerHTML += `
+        <tr>
+            <td>${productoComprado.id}</td>
+            <td>${productoComprado.nombre}</td>
+            <td>${productoComprado.precio}</td>
+        </tr>
+    `;
+    totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText = " El monto Total a pagar $: " + totalCarrito;
 }
-*/
-//añadiendo arrays de objetos a mi proyecto post clase 6
-
-const goleadores = [{
-        nombre: 'benedetto',
-        goles: 5,
-        detalle: '5 goles en 15 partidos'
-    },
-    {
-        nombre: 'retegui',
-        goles: 12,
-        detalle: '12 goles en 20 partidos'
-    },
-    {
-        nombre: 'boselli',
-        goles: 8,
-        detalle: '8 goles en 16 partidos'
-    },
-    {
-        nombre: 'copetti',
-        goles: 10,
-        detalle: '10 goles en 19 partidos'
-    },
-    {
-        nombre: 'cristaldo',
-        goles: 5,
-        detalle: '5 goles en 19 partidos'
-    },
-    {
-        nombre: 'langoni',
-        goles: 6,
-        detalle: '6 goles en 9 partidos'
-    }
-];
-
-let busqueda = prompt('ingrerse el apellido del jugador que busca en miniscula')
-
-
-//console.log(goleadores);
-/*
-for(const goleador of goleadores){
-  console.log(goleador);
-};
-*/
