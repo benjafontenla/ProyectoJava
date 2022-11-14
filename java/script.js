cff8ba5c0f606e450d86062f2be64559c5adb4b7
benja fontenla <bfontenla5@gmail.com>
2022-11-14 19:44:07 -0300

    preentrega

--- java/script.js
+++ java/script.js
@@ -1,14 +1,50 @@
+let productosJSON = [];
 //Armo la const 'carrito' para usar a la hora de realizar la function.
 const carrito = [];
-
 //variable global 
 let totalCarrito;
+//Tomo el nodo contenedor para la tarjeta de productos
+let contenedor = document.getElementById("Mis Productos"); //Padre
+//Esroy tomando el id ' finalizar.
+let botonFinalizar = document.getElementById("finalizar")
+//Para preguntar si hay algun carrito guardado (operador logico or)
+let carritos = JSON.parse(localStorage.getItem('carritos')) || [];
 
-//Tomo el nodo 
-let contenedor = document.getElementById("Mis productos"); //Padre
-//Armo la function y las tarjetas de productos
+
+//renderizado para retomar un carrito abandonado.
+//(carrito.length != 0) && dibujarTabla();
+//o 
+if (carrito.length != 0) {
+    console.log('recuperando carro')
+    dibujarTabla();
+}
+
+
+//function dibujarTabla
+function dibujarTabla() {
+    for (const producto of carrito) {
+        document.getElementById(' tablabody').innerHTML += `
+        <tr>
+        <td>${producto.id}</td>
+        <td>${producto.nombre}</td>
+        <td>${producto.precio}</td>
+        <td>button class='btn btn-light' onclick= 'eliminar(event)'> </button></td>
+        
+        </tr>
+        `;
+    }
+    //Acumulamos el total con reduce.
+    totalCarrito = carritos.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText = " El monto Total a pagar $: " + totalCarrito;
+}
+
+
+
+//Function renderizarProds()
+
 function catalogo(){
-    for(const producto of productos){
+    for(const producto of productosJSON){
         //Uso una plantilla literal. Uso += para que la carta se gregue a la anterior
         contenedor.innerHTML += ` 
             <div class="card col-sm-2">
@@ -22,24 +58,29 @@ function catalogo(){
             </div>
         `;
     }
-
-//Eventos
-    productos.forEach(producto => {
+    //Eventos
+    productosJSON.forEach(producto => {
         //Evento para cada boton. Tomo el boton y agrego el escuchador de evento.
-        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){ //Escuhador de evento.
+        document.getElementById(`btn${producto.id}`).addEventListener("click", function () { //Escuhador de evento.
             agregarAlCarrito(producto); //Dentro de la function anonima llamo a 'agregarAlCarrito'
         });
     })
 }
 
 
-catalogo();
-//Armo la function del segundo parametro.
-function agregarAlCarrito(productoComprado){
+//Armo la function del segundo parametro. (31)
+function agregarAlCarrito(productoComprado) {
     carrito.push(productoComprado);
     console.table(carrito);
-    //Tengo que cambiar este alert
-    alert("Producto: "+productoComprado.nombre+" agregado al carrito!");
+    alert("Producto: " + productoComprado.nombre + " agregado al carrito!");
+    swal.fire({
+title: productoComprado.nombre,
+text: 'agregando al carrito',
+imageWidth: 200,
+imageAlt: productoComprado.nombre,
+showconfirmButton: false,
+timer: 1500
+    })
     //plantilla literal.
     document.getElementById("tablabody").innerHTML += `
         <tr>
@@ -49,15 +90,12 @@ function agregarAlCarrito(productoComprado){
         </tr>
     `;
     //Acumulamos el total con reduce.
-    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
     let infoTotal = document.getElementById("total");
-    infoTotal.innerText=" El monto Total a pagar $: "+totalCarrito;
+    infoTotal.innerText = " El monto Total a pagar $: " + totalCarrito;
 
-    //Almaceno los productos pusheados al carrito por el usuario.
-    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-/*Almaceno en el localStorage mi lista de productos.
-const productosAJason = JSON.stringify(productos);
-localStorage.setItem('productos', productosAJason);
-*/
+
+
+
